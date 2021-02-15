33a6fa6140222e5c3e8c08c64a425892e10334dc
Oliver Rosales <oliverjohnpr2013@gmail.com>
2021-02-15 16:39:12 +0800

    Fixes

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -18,7 +18,6 @@ describe('Login feature', function(){
                 build();
     
             // Navigate to Url
-            // await driver.get("http://host.docker.internal:3001");
             await driver.get("http://web_app:3000");
             let message = await driver.findElement(By.id("title")).getText();
             console.log("The message is", message);
